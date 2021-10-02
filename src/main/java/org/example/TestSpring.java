b76facc566013c88c90fcfd85201fe7f723d3cd4
lilkaPTS <lilgud@mail.ru>
2021-10-02 23:30:14 +0300

    Inversion of Control

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -5,8 +5,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         //обращение к файлу ресурсов апликейшен контекст и помещает все бины описанные в контекст
+        System.out.println("ApplicationContext");
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        System.out.println(context.getBean("testBean", TestBean.class).getName());
+        System.out.println(context.getBean("testBean", TestBean.class).getName() + "\n");
+
+        System.out.println("Inversion of Control");
+        Music music = context.getBean("classicalMusicBean", Music.class);
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+        musicPlayer.playMusic();
+        musicPlayer = new MusicPlayer(context.getBean("rockMusicBean", Music.class));
+        musicPlayer.playMusic();
         context.close();
     }
 }
